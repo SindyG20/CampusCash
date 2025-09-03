dd609a8304f44940d10a423fd465fa2bc32787e8
SindyG20 <ngimphiwesenamile0@gmail.com>
2025-09-03 08:13:58 +0200

    Connected the log in and register page to the database again...

--- MyAspMySql/obj/Debug/net9.0/MyAspMySql.AssemblyInfo.cs
+++ MyAspMySql/obj/Debug/net9.0/MyAspMySql.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAspMySql")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2978dd8f147a74c9f7180b528f356b5ec5e3e696")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d75d061442ef4ae89575da13ea9eefe8e140df2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAspMySql")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAspMySql")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
